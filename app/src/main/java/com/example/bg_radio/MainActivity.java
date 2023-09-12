3513405114a7ac6b53a888ad1873d9a20aca0d3e
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2023-09-12 22:47:17 +0300

    .

--- app/src/main/java/com/example/bg_radio/MainActivity.java
+++ app/src/main/java/com/example/bg_radio/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.media3.common.Player;
 import androidx.media3.exoplayer.ExoPlayer;
 import androidx.media3.ui.PlayerView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
