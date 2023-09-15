554d5ccfbfaac7e91c3b517453d7858144d73f69
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2023-09-15 14:51:53 +0300

    Added Timer for displaying Title and Artist on interval

--- app/src/main/java/com/example/bg_radio/MainActivity.java
+++ app/src/main/java/com/example/bg_radio/MainActivity.java
@@ -3,12 +3,12 @@ package com.example.bg_radio;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.MediaMetadata;
-import androidx.media3.common.Player;
 import androidx.media3.exoplayer.ExoPlayer;
-import androidx.media3.ui.PlayerView;
 
-import android.net.Uri;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -16,21 +16,67 @@ import android.widget.TextClock;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
     String linkBg = "http://play.global.audio/bgradio.ogg";
     String link1rock = "http://149.13.0.81/radio1rock.ogg";
-    String link = linkBg;
     TextView viewSong;
-    PlayerView viewPlayer;
+    TextView viewArtist;
     TextClock clock;
     ExoPlayer player;
     Button btnBgRadio;
     Button btn_1rock;
     List<MediaItem> stations = new ArrayList<>();
+    MediaMetadataRetriever retriever;
+    String title;
+    String[] titleSplit = {"No data", "No data"};
+    Timer timer = new Timer();
+    String currentUrl = link1rock;
+
+        @Override
+                protected void onResume(){
+            super.onResume();
+            TimerTask timerTask = new TimerTask() {
+                @Override
+                public void run() {
+                    String[] data = getData(currentUrl);
+                    displayData(data);
+                }
+            };
+            timer.scheduleAtFixedRate(timerTask, 0, 5000);
+
+        }
+
+        public void displayData(String[] data){
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    viewSong.setText(data[1]);
+                    viewArtist.setText(data[0]);
+                }
+            });
+
+
+        }
+    public String[] getData(String link) {
+        retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(link, new HashMap<>());
+        title = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        if (title != null) titleSplit = title.split(" - ", 2);
+
+        try {
+            retriever.release();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return titleSplit;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,64 +86,52 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         viewSong = findViewById(R.id.song);
-        viewPlayer = findViewById(R.id.player);
+        viewArtist = findViewById(R.id.artist);
         clock = findViewById(R.id.textClock);
         btnBgRadio = findViewById(R.id.btnbgradio);
         btn_1rock = findViewById(R.id.btn1rock);
 
-        ExoPlayer.Builder builder = new ExoPlayer.Builder(this);
-        player = builder.build();
-        viewPlayer.setPlayer(player);
+        player = new ExoPlayer.Builder(this).build();
 
-        MediaMetadata mediaMetadata = new MediaMetadata.Builder().
-                setDisplayTitle("1RockTitle").
-                setArtist("1RockArtist").build();
+        MediaMetadata mediaMetadata = new MediaMetadata.Builder().build();
 
         MediaItem mediaItem = new MediaItem.Builder().
                 setMediaMetadata(mediaMetadata)
-                .setUri("http://149.13.0.81/radio1rock.ogg")
+                .setUri(link1rock)
                 .build();
 
         stations.add(mediaItem);
 
-        mediaMetadata = new MediaMetadata.Builder().
-                setDisplayTitle("BRGadioTitle").
-                setArtist("BGRadioArtist").build();
+        mediaMetadata = new MediaMetadata.Builder().build();
 
         mediaItem = new MediaItem.Builder().
                 setMediaMetadata(mediaMetadata)
-                .setUri("http://149.13.0.81/bgradio.ogg")
+                .setUri(linkBg)
                 .build();
 
         stations.add(mediaItem);
 
-        player.addListener(new Player.Listener() {
-                    @Override
-                    public void onMediaMetadataChanged(MediaMetadata mediaMetadata) {
-                        String title = mediaMetadata.displayTitle.toString();
-                        viewSong.setText(title);
-                    }
-                }
-        );
         player.setMediaItems(stations);
+        player.prepare();
+        player.setPlayWhenReady(true);
 
         btn_1rock.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                MediaItem item = stations.get(0);
-                player.setMediaItem(item);
-                player.prepare();
-                player.play();
+                player.seekTo(0, 0);
+                currentUrl = link1rock;
+                titleSplit = getData(currentUrl);
+                displayData(titleSplit);
             }
         });
 
         btnBgRadio.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                MediaItem item = stations.get(1);
-                player.setMediaItem(item);
-                player.prepare();
-                player.play();
+                player.seekTo(1, 0);
+                currentUrl = linkBg;
+                titleSplit = getData(currentUrl);
+                displayData(titleSplit);
             }
         });
 
@@ -109,3 +143,6 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 }
+
+
+
